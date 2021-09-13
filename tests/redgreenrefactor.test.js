f630ede0f19d9327c393722b8c64992f90cc19cf
Aemilius Morgan <aemiliusmorgan@gmail.com>
2021-09-13 12:02:59 -0700

    set up failing test

---
+++ tests/redgreenrefactor.test.js
@@ -0,0 +1,12 @@
+// const spot = { name: 'spot', age: 5, weight: '20 lbs' };
+// getName(spot); // returns spot
+
+// const character = { _id: '5cf5679a915ecad153ab68c9', name: 'Aang' };
+// getName(character); // returns Aang
+
+describe('getName', () => {
+  it('grabs the name from an object', () => {
+    const spot = { name: 'spot', age: 5, weight: '20 lbs' };
+    expect(getName(spot)).toEqual('spot'); // returns spot
+  });
+});
