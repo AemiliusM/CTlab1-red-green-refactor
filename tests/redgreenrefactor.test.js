ad878462484dcad447a073591311a365b3eb2263
Aemilius Morgan <aemiliusmorgan@gmail.com>
2021-09-13 13:11:24 -0700

    passing bracket test

--- tests/redgreenrefactor.test.js
+++ tests/redgreenrefactor.test.js
@@ -1,3 +1,5 @@
+import getName from '../functions/redgreenrefactor.js';
+
 // const spot = { name: 'spot', age: 5, weight: '20 lbs' };
 // getName(spot); // returns spot
 
@@ -8,5 +10,9 @@ describe('getName', () => {
   it('grabs the name from an object', () => {
     const spot = { name: 'spot', age: 5, weight: '20 lbs' };
     expect(getName(spot)).toEqual('spot'); // returns spot
+  });
+  it('grabs the name from an object', () => {
+    const character = { _id: '5cf5679a915ecad153ab68c9', name: 'Aang' };
+    expect(getName(character)).toEqual('Aang'); // returns Aang
   });
 });
